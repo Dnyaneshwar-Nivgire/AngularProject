64e40c3878ddae7c2478d3747f4aa65925d519f1
Dnyaneshwar-Nivgire <dnivgire1@gmail.com>
2024-09-24 11:24:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LearnDirectives';
+  color="yellow";
+  
+  data:any={
+    username:"atul",
+    password:"12334"
+  }
+   
+  numbers:any=[1,2,3,4] 
+
+  students:any=[
+    {
+    id:1,
+    name:"Ram",
+    age:23
+  },
+  {
+    id:2,
+    name:"sham",
+    age:24
+  },
+
+];
+
+//Attributes directives
+
+isHighlight=false;
+highlightOnOff(){
+  this.isHighlight =!this.isHighlight;
+
+}
+fontSize='32px';
+increase(){
+  this.fontSize=parseInt(this.fontSize)+4+'px';
+}
+decrease(){
+  this.fontSize=parseInt(this.fontSize)-4+'px';
+}
 }
